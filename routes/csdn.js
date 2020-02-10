889eff2ee9b68828daafd94f8731f5d7d720421c
Tuzilow <1176281967@qq.com>
2020-02-10 17:29:19 +0800

    完成分页获取

--- routes/csdn.js
+++ routes/csdn.js
@@ -1,67 +1,82 @@
 const cheerio = require('cheerio');
 const superagent = require('superagent');
 const express = require('express');
+const eventproxy = require('eventproxy');
 
 const router = express.Router();
+const ep = new eventproxy();
 
 router.get('/csdn/:name', function (req, res) {
-    let name = req.params.name;
+    let name = req.params.name; // 用户账号
 
-    getPageNum(name, function (num) {
-        for (let index = 1; index <= num; index++) {
-            let targetUrl = `https://blog.csdn.net/${name}/article/list/${index}`;
-            console.log(targetUrl);
+    getArticleNum(`https://blog.csdn.net/${name}`, function (num) {
+        let articleData = []; // 保存所有文章数据
+        let pages = []; // 保存要抓取的页面
+
+        let pageNum = Math.ceil(num / 40); // 计算一共有多少页面
+
+        for (let i = 1; i <= pageNum; i++) {
+            pages.push(`https://blog.csdn.net/${name}/article/list/${i}?t=1`);
+        }
+
+        pages.forEach(function (targetUrl) {
             superagent.get(targetUrl).end(function (err, html) {
                 if (err) {
-                    console.log('抓取失败');
+                    console.log(`err $err}`);
                 }
+                let $ = cheerio.load(html.text);
 
-                data.push(getArticle(html))
+                let articlesHtml = $('.article-list .article-item-box');
 
-                res.status(200).json({
-                    status: 0,
-                    data: data
-                });
+                for (let i = 0; i < articlesHtml.length; i++) {
+                    let article = analysisHtml(articlesHtml, i);
+
+                    articleData.push(article);
+                    ep.emit('blogArtc', article);
+                }
             });
-        }
-    });
+        });
+        ep.after('blogArtc', num, function (data) {
+            res.json({
+                status_code: 0,
+                data: data
+            });
+        });
+    })
+
 });
 
-// 解析html字符串，获取文章信息
-let getArticle = function (res) {
-    let $ = cheerio.load(res.text);
-    let data = [];
-
-    $('.article-list .article-item-box').each(function (i, elem) {
-        data.push({
-            id: $(elem).attr('data-articleid'),
-            article_type: $(elem).find('h4 a').children().text(),
-            title: $(elem).find('h4 a').text().replace(/\s+/g, '').slice(2),
-            link: $(elem).find('a').attr('href'),
-            abstract: $(elem).find('.content a').text().replace(/\s+/g, ''),
-            shared_time: $(elem).find('.info-box .date').text().replace(/\s+/, ''),
-            read_count: $(elem).find('.info-box .read-num .num').first().text().replace(/\s+/, ''),
-            comment_count: $(elem).find('.info-box .read-num .num').last().text().replace(/\s+/, '')
-        });
-    });
-
-    return data;
-}
-
-// csdn的分页是js生成的，无法直接获得
-// 从主页获取文章总数后计算分页
-let getPageNum = function (name, callback) {
-    let url = `https://me.csdn.net/${name}`;
-    superagent.get(url).end(function (err, res) {
+/**
+ * 读取页面
+ * @param {String} url 解析路径
+ */
+let getArticleNum = function (url, callback) {
+    superagent.get(url).end(function (err, html) {
         if (err) {
-            console.log('分页获取失败');
+            console.log(`err = ${err}`);
         }
-        // console.log(res.text)
-        let $ = cheerio.load(res.text);
-        let num = parseInt($('.me_chanel_bar .tab_item .count').text());
-        num = Math.ceil(num / 40);
+        let $ = cheerio.load(html.text);
+        let num = parseInt($('.data-info dl').first().attr('title'));
+
         callback(num);
     });
-}
+};
+
+/**
+ * 解析html字符串，获取文章信息
+ * @param {String} html 包含文章的html
+ */
+let analysisHtml = function (html, index) {
+    return {
+        id: html.eq(index).attr('data-articleid'),
+        article_type: html.eq(index).find('h4 a').children().text(),
+        title: html.eq(index).find('h4 a').text().replace(/\s+/g, '').slice(2),
+        link: html.eq(index).find('a').attr('href'),
+        abstract: html.eq(index).find('.content a').text().replace(/\s+/g, ''),
+        shared_time: html.eq(index).find('.info-box .date').text().replace(/\s+/, ''),
+        read_count: html.eq(index).find('.info-box .read-num .num').first().text().replace(/\s+/, ''),
+        comment_count: html.eq(index).find('.info-box .read-num .num').last().text().replace(/\s+/, '')
+    };
+};
 
 module.exports = router;
